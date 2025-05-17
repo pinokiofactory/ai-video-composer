77a9f473211f33334bca66e25d35ec50f3b800b7
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-05-17 04:14:24 +0200

    pin pydantic

--- install.js
+++ install.js
@@ -16,7 +16,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install -r requirements.txt"
+          "uv pip install -r requirements.txt",
+          "uv pip install pydantic==2.10.6"
         ]
       }
     },
